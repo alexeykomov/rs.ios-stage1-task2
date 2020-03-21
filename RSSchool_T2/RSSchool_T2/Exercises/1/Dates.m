7d69c23eaa69f3c35766afdcddca90e5ecf1ab59
alexeykomov <alexeykofficial@gmail.com>
2020-03-21 15:08:39 +0300

    feat: complete date formatter task

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,30 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateFormat = @"dd MMMM, EEEE";
+    fmt.locale = [NSLocale localeWithLocaleIdentifier:@"ru_RU_POSIX"];
+    fmt.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:3600 * 3];
+    
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    comps.day = [day intValue];
+    comps.month = [month intValue];
+    comps.year = [year intValue];
+    NSCalendar *cal = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    NSDate *date = [cal dateFromComponents:comps];
+    
+    NSDateComponents *appliedComps = [cal components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:date];
+    
+    if (appliedComps.day != [day intValue] || appliedComps.month != [month intValue] ||
+        appliedComps.year != [year intValue]) {
+        return @"Такого дня не существует";
+    }
+    
+    NSLog(@"Date: %@", date);
+    
+    NSString *output = [fmt stringFromDate:date];
+    NSLog(@"Output: %@", output);
+    return output;
 }
 
 @end
