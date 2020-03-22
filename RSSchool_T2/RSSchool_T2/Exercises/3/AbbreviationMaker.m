55149d4511f7f1dc128a89eb6dcdc381746a4cfb
alexeykomov <alexeykofficial@gmail.com>
2020-03-22 14:15:37 +0300

    feat: complete abbreviation task

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,30 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    
+    int counterA = 0;
+    int counterB = 0;
+    while ((counterA < [a length]) && (counterB < [b length])) {
+        NSRange rB = NSMakeRange(counterB, 1);
+        NSRange rA = NSMakeRange(counterA, 1);
+        NSString *symbolB = [b substringWithRange:rB];
+        NSString *symbolA = [a substringWithRange:rA];
+        if ([symbolB isEqualToString:symbolA]) {
+            counterB++;
+            counterA++;
+            continue;
+        }
+        if (![symbolB isEqualToString:symbolA]) {
+            if ([symbolB isEqualToString:[symbolA uppercaseString]]) {
+                counterB++;
+                counterA++;
+                continue;
+            }
+            counterA++;
+        }
+    }
+    
+    return counterB == [b length] ? @"YES" : @"NO";
+    
 }
 @end
